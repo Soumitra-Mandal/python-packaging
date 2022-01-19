5d915fc6f1a895b219f5134257038d9dde3131cf
Soumitra Mandal <soumitramandal1999@gmail.com>
2022-01-19 12:53:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 """
 Created on Thu Jul  4 12:16:50 2019
 
@@ -7,25 +9,22 @@ Created on Thu Jul  4 12:16:50 2019
 
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="soumi",
-    version="1.1.1",
-    author="Soumitra Mandal",
-    author_email="soumitramandal1999@gmail.com",
-    description="A small package to help with simple mathematical operations",
+    name='soumi',
+    version='1.1.1',
+    author='Soumitra Mandal',
+    author_email='soumitramandal1999@gmail.com',
+    description='A small package to help with simple mathematical operations'
+        ,
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Soumitra-Mandal/python-packaging",
+    long_description_content_type='text/markdown',
+    url='https://github.com/Soumitra-Mandal/python-packaging',
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-          'matplotlib',
-          ],
-)
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent'],
+    install_requires=['matplotlib'],
+    )
